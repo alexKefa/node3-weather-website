7919f3581842b95640f5a0b64c98ebb0ecf03a0b
Alexandros Kefalas <alexkef1999@gmail.com>
2019-09-20 07:10:24 +0300

    Added new weather information for the weather app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location!', undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently '+ (body.currently.temperature) +' degrees out. There is a '+(body.currently.precipProbability)+'% chance of rain.')
+            callback(undefined, body.daily.data[0].summary + ' It is currently '+ (body.currently.temperature) +' degrees out. There is a '+(body.currently.precipProbability)+'% chance of rain. The highest temperature for today is ' + body.daily.data[0].temperatureHigh +' degrees and the lowest is ' +body.daily.data[0].temperatureLow + ' degrees.')
         }
     })
 }
